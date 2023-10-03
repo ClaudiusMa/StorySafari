6fba93fa6447c24ba2f7ee9b6a154fc0c9e663b6
Claudius Ma <heiclaudius@gmail.com>
2023-10-03 12:05:56 -0700

    fix

--- frontend/src/pages/api/call-gpt.ts
+++ frontend/src/pages/api/call-gpt.ts
@@ -4,15 +4,21 @@ import Metaphor from "metaphor-node"
 const metaphor = new Metaphor(process.env.METAPHOR_API_KEY)
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    const { companyName } = req.body;
-    const response = await metaphor.search(`Here is a portfolio of a designer who works at ${companyName}`, {
-      numResults: 10,
-    });
+  if (req.method === 'POST') {
+    // Handle POST request
+    try {
+      const { companyName } = req.body;
+      const response = await metaphor.search(`Here is a portfolio of a designer who works at ${companyName}`, {
+        numResults: 10,
+      });
 
-    res.status(200).json(response);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: error.message });
+      res.status(200).json(response);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: error.message });
+    }
+  } else {
+    // Handle other methods or return an error
+    res.status(405).json({ message: 'Method Not Allowed' });
   }
 }
